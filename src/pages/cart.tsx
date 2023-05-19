f9b8279a2dc6865c7089d83da46b7155f76fbfd8
alek4 <alessandro.bordo41@gmail.com>
2023-05-19 15:56:59 +0200

    fix hydration error on isEmpty cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -5,22 +5,34 @@ import { useEffect, useState } from "react";
 import Product from "@/utils/ProductType";
 
 export default function Cart() {
-  const { items, emptyCart, removeItem, updateItemQuantity } =
+  const { items, emptyCart, removeItem, updateItemQuantity, isEmpty } =
     useCart();
+  
   const [allItems, setallItems] = useState<any>([{}]);
+  const [_isEmpty, setisEmpty] = useState<boolean>(true);
+
   useEffect(() => {
     setallItems(JSON.parse(JSON.stringify(items)));
   }, [items]);
 
+
+  useEffect(() => {
+    setisEmpty(JSON.parse(JSON.stringify(isEmpty)));
+  }, [isEmpty]);
+
   return (
-    <>
-      <Wrapper>
-        ziopera_
-        {allItems.map((prod: any, i: any) => (
-          <div key={i}>{prod.name + " " + prod.quantity}</div>
-        ))}
+    <div className="md:h-screen bg-[#f2f0ed] pt-24">
+      <Wrapper className="flex flex-col">
+        <h1 className="text-6xl font-bold mb-5 uppercase">Carrello</h1>
+        {_isEmpty ? (
+          <h1 className="text-xl font-bold uppercase text-zinc-400 text-center mt-10">Il tuo Carrello è vuoto, corri a fare shopping!</h1>
+        ) : (
+          allItems.map((prod: any, i: any) => (
+            <div key={i}>{prod.name + " " + prod.quantity}</div>
+          ))
+        )}
       </Wrapper>
       <NavBar></NavBar>
-    </>
+    </div>
   );
 }
