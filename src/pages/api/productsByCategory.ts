727d981f2596faee19662e89ba0d1a406f265b13
alek4 <alessandro.bordo41@gmail.com>
2023-09-25 16:10:42 +0200

    solve import error 2

--- src/pages/api/productsByCategory.ts
+++ src/pages/api/productsByCategory.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Product } from "@/utils/ProductType";
 import type { NextApiRequest, NextApiResponse } from "next";
-import data from "public/data/data.json";
+import data from "../../../public/data/data.json";
 
 export default function handler(
   req: NextApiRequest,
@@ -9,13 +8,13 @@ export default function handler(
 ) {
   if (req.method === 'GET') {
     const { category } = req.query;
-    
+
 
     if (!category || typeof category !== 'string') {
       return res.status(400).json({ error: 'Invalid category parameter' });
     }
 
-    const products = data.filter((p: Product) => p.category === category);
+    const products = data.filter((p: any) => p.category === category);
 
     if (!products) {
       return res.status(404).json({ error: 'Products not found' });
