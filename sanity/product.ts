ad7684831583976730840ca4b651173861c96416
Alessandro Bordonali <alessandro.bordo41@gmail.com>
2024-05-31 14:39:49 +0200

    fix type error

--- sanity/product.ts
+++ sanity/product.ts
@@ -64,7 +64,7 @@ export default {
         title: "Type",
         type: 'array',
         of: [{type: 'string'}],
-        hidden: ({ parent, value }) => parent?.category !== "accessories"
+        hidden: ({ parent, value }: any) => parent?.category !== "accessories"
     }
     ]
 }
